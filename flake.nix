2ab433e3cb1215196976a25d2860c89a48886f16
qirpi <qirpi@lambd.ax>
2023-03-04 17:20:03 +0100

    add nix runnable bootsrap script and clean thingz up

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   description = "Rails 7 flake template with dev tools included";
 
-  outputs = { self, ... }: {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
     templates = {
       rails-7 = {
         path = ./rails-7;
@@ -11,5 +15,35 @@
     };
 
     templates.default = self.templates.rails-7;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.bootstrap;
+    packages.x86_64-linux.bootstrap =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+      pkgs.writeShellScriptBin "bootstrap" ''
+
+        mkdir $PROJECT_NAME && cd $PROJECT_NAME
+        git init --quiet && git branch -M main
+        nix flake init --template github:qirpi/nix-rails7
+        git add .
+
+        direnv allow .
+        direnv exec . ruby -v
+        direnv exec . rails --version
+
+        echo
+        echo "______________________________________________________________"
+        echo
+        echo "  Your brand new, flake based Rails devenv is ready.."
+        echo 
+        echo
+        echo "  Next: run 'rails new .' inside the project folder."
+        echo
+        echo "  Have fun!" 
+        echo "______________________________________________________________"
+        echo
+
+      '';
   };
 }
